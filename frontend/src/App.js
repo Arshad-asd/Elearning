958553e940c11b92e37d0658c14e69763abf021b
Arshad <arshaarshad21@gmail.com>
2023-09-17 11:53:12 +0530

    App.js created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-   E learning
+       <Navbar />
     </div>
   );
 }
