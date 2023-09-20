87af3290d018a6da9f74f1669299ed9ce8285009
Arshad <arshaarshad21@gmail.com>
2023-09-20 17:08:22 +0530

    AdminDashboard.js create

---
+++ frontend/src/Containers/admin/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../Containers/admin/AdminDashboard.css'
+
+function AdminDashboard() {
+  return (
+    <div className='grid-container'>
+        <h1>Admin boody</h1>
+    </div>
+  )
+}
+
+export default AdminDashboard
