4b6ea8dd1a93af458d4b39b169d94589c1d98664
Arshad <arshaarshad21@gmail.com>
2023-09-20 17:08:46 +0530

    Router.js file update

--- frontend/src/Containers/routes/Routers.js
+++ frontend/src/Containers/routes/Routers.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
+import Home from '../user/Home'
+import AdminDashboard from '../admin/AdminDashboard'
+import TutorDashboard from '../tutor/TutorDashboard'
 
 const Routers = () => {
   return (
     <Routes>
-        <Route />
+        <Route path="/" element={<Home />}/>
+
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        <Route path="/tutor" element={<TutorDashboard />} />
+
     </Routes>
   )
 }
