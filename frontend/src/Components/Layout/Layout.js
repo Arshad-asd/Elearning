a5f3b79582c038322087a6fd615d90f4a15c46fc
Arshad <arshaarshad21@gmail.com>
2023-09-20 17:05:00 +0530

    layout.js file update

--- frontend/src/Components/Layout/Layout.js
+++ frontend/src/Components/Layout/Layout.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Routers from "../../Containers/routes/Routers";
+import { useLocation } from "react-router-dom";  // Its mainly used for finding preferd location
+import AdminSidebar from "../../Components/Sidebar/AdminSidebar";
+import TutorSidebar from "../../Components/Sidebar/TutorSidebar";
 function Layout() {
+    let location = useLocation()
+    let tutorHeader = location.pathname.startsWith('/tutor')
+    let adminHeader = location.pathname.startsWith('/admin')
   return (
-    <div>Layout</div>
-  )
+    <>
+    {
+        (tutorHeader)?<TutorSidebar />:((adminHeader)?<AdminSidebar />:<Navbar />) // Its mainly used for checking its navbar user ,tutor or admin
+    }
+      
+      <Routers />
+ 
+      
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
