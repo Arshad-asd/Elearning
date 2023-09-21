d53f7c03a2248d0de2efa7481ed876b8c81e5e34
Arshad <arshaarshad21@gmail.com>
2023-09-21 11:29:14 +0530

    Update Router.js

--- frontend/src/Containers/routes/Routers.js
+++ frontend/src/Containers/routes/Routers.js
@@ -3,10 +3,14 @@ import {Routes,Route} from 'react-router-dom'
 import Home from '../user/Home'
 import AdminDashboard from '../admin/AdminDashboard'
 import TutorDashboard from '../tutor/TutorDashboard'
+import Login from '../user/Login'
+import Register from '../user/Register'
 
 const Routers = () => {
   return (
     <Routes>
+        <Route path='/signup' element={<Register />} />
+        <Route path='/login' element={<Login/>} />
         <Route path="/" element={<Home />}/>
 
         <Route path="/admin" element={<AdminDashboard />} />
