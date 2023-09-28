10147a505364366774e1a9609f1a1a504d9bce98
Arshad <arshaarshad21@gmail.com>
2023-09-28 21:32:45 +0530

    update

--- frontend/src/Components/Sidebar/AdminSidebar.js
+++ frontend/src/Components/Sidebar/AdminSidebar.js
@@ -1,13 +1,44 @@
-import React ,{ useState } from 'react'
-import '../../Components/Sidebar/AdminSidebar.css'
+// AdminSidebar.js
+import React, { useState } from 'react';
+import '../../Components/Sidebar/AdminSidebar.css';
+import { FaHome, FaUser, FaChalkboardTeacher, FaBook, FaMoneyBillAlt } from 'react-icons/fa';
 
 function AdminSidebar() {
-  
+  const [isIconsOnly, setIsIconsOnly] = useState(false);
+
+  const toggleIconsOnly = () => {
+    setIsIconsOnly(!isIconsOnly);
+  };
 
   return (
-    <>
-    </>
+    <aside className={`admin-sidebar ${isIconsOnly ? 'icons-only' : ''}`}>
+      <div className="toggle-button" onClick={toggleIconsOnly}>
+        {isIconsOnly ? '☰' : '✖'}
+      </div>
+      <ul>
+        <li>
+        <FaHome className="sidebar-icon" />
+          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Dashboard</span>
+        </li>
+        <li>
+          <FaUser className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>User Management</span>
+        </li>
+        <li>
+          <FaChalkboardTeacher className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Tutor Management</span>
+        </li>
+        <li>
+          <FaBook className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Courses</span>
+        </li>
+        <li>
+          <FaMoneyBillAlt className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Subscription</span>
+        </li>
+      </ul>
+    </aside>
   );
+}
 
-  }
-export default AdminSidebar
+export default AdminSidebar;
