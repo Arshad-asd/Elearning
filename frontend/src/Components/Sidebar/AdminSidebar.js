567a05a9858176f88a7a7bb039b0032e3e23cdb9
Arshad <arshaarshad21@gmail.com>
2023-09-30 22:21:40 +0530

    update

--- frontend/src/Components/Sidebar/AdminSidebar.js
+++ frontend/src/Components/Sidebar/AdminSidebar.js
@@ -1,7 +1,8 @@
 // AdminSidebar.js
 import React, { useState } from 'react';
 import '../../Components/Sidebar/AdminSidebar.css';
-import { FaHome, FaUser, FaChalkboardTeacher, FaBook, FaMoneyBillAlt } from 'react-icons/fa';
+import { FaHome, FaUser, FaChalkboardTeacher, FaBook, FaMoneyBillAlt, FaChartBar, FaEnvelope,  FaSignOutAlt } from 'react-icons/fa';
+import AdminHeader from '../../Components/Header/AdminHeader';
 
 function AdminSidebar() {
   const [isIconsOnly, setIsIconsOnly] = useState(false);
@@ -9,35 +10,58 @@ function AdminSidebar() {
   const toggleIconsOnly = () => {
     setIsIconsOnly(!isIconsOnly);
   };
-
+  
+  
+  const handleLogout = () => {
+    // Add logic for logging out
+    console.log('Logout clicked');
+  };
+  
   return (
-    <aside className={`admin-sidebar ${isIconsOnly ? 'icons-only' : ''}`}>
-      <div className="toggle-button" onClick={toggleIconsOnly}>
-        {isIconsOnly ? '☰' : '✖'}
-      </div>
-      <ul>
-        <li>
-        <FaHome className="sidebar-icon" />
-          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Dashboard</span>
-        </li>
-        <li>
-          <FaUser className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
-          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>User Management</span>
-        </li>
-        <li>
-          <FaChalkboardTeacher className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
-          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Tutor Management</span>
-        </li>
-        <li>
-          <FaBook className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
-          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Courses</span>
-        </li>
-        <li>
-          <FaMoneyBillAlt className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
-          <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Subscription</span>
-        </li>
-      </ul>
-    </aside>
+    <>
+      <AdminHeader />
+      <aside className={`admin-sidebar ${isIconsOnly ? 'icons-only' : ''}`}>
+        <div className="toggle-button" onClick={toggleIconsOnly}>
+          {isIconsOnly ? '☰' : '✖'}
+        </div>
+        <ul>
+          <li>
+            <FaHome className="sidebar-icon" />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Dashboard</span>
+          </li>
+          <li>
+            <FaUser className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>User Management</span>
+          </li>
+          <li>
+            <FaChalkboardTeacher className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Tutor Management</span>
+          </li>
+          <li>
+            <FaBook className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Courses</span>
+          </li>
+          <li>
+            <FaMoneyBillAlt className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Subscription</span>
+          </li>
+
+          {/* New li tags with corresponding icons */}
+          <li>
+            <FaChartBar className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Sales Report</span>
+          </li>
+          <li>
+            <FaEnvelope className={`sidebar-icon ${isIconsOnly ? 'hidden' : ''}`} />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Messages</span>
+          </li>
+          <li className="logout-button" onClick={handleLogout}>
+            <FaSignOutAlt className="sidebar-icon" />
+            <span className={`menu-text ${isIconsOnly ? 'hidden' : ''}`}>Logout</span>
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 }
 
