6ad3bc5d85eeb896789e7684039eb9403fc326bf
Arshad <arshaarshad21@gmail.com>
2023-10-27 10:54:00 +0530

    update footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,83 +1,61 @@
-import React from 'react'
-import '../../Components/Footer/Footer.css'
-import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs'
+import React from "react";
+import "../../Components/Footer/Footer.css";
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+import img from "../../assets/ElearningLogo.svg";
 function Footer() {
   return (
-    <div className='footer'>
-      <div className='sb_footer_padding'>
-        <div className='sb_footer-links'>
-          <div className='sb_footer-links_div'>
-            <h4>for Business</h4>
-             <a href=''>
-                <p>Employer</p>
-             </a>
-             <a href=''>
-                <p>Health plan</p>
-             </a>
-             <a href=''>
-                <p>Individual</p>
-             </a>
+    <footer className="footer">
+      <div className="row">
+        <div className="column">
+          <div>
+            <img src={img} alt="Logo" className="logo-image" />
           </div>
-          <div className='sb_footer-links_div'>
-            <h4>for Business</h4>
-            <a href=''>
-                <p>Resource center</p>
-             </a>
-             <a href=''>
-                <p> Testimonils</p>
-             </a>
-             <a href=''>
-                <p>Individual</p>
-             </a>
+          <div>
+            <p className="paragraph">
+              is the delivery of learning and training through digital
+              resources. Although eLearning is based on formalized learning.
+            </p>
           </div>
-          <div className='sb_footer-links_div'>
-          <h4> Partners</h4>
-          <a href=''>
-                <p>Swing tech</p>
-             </a>
         </div>
-        <div className='sb_footer-links_div'>
-          <h4> Company</h4>
-          <a href=''>
-                <p> About</p>
-             </a>
-             <a href=''>
-                <p> Press</p>
-             </a>
-             <a href=''>
-                <p>Career</p>
-             </a>
-             <a href=''>
-                <p>Contact</p>
-             </a>
+        <div className="column">
+          <span style={{ fontFamily: "Bebas Neue, sans-serif" }}>Company</span>
+
+          <div className="mt-3">About Us</div>
+          <div className="mt-3">How to work?</div>
+          <div className="mt-3">Populer Course</div>
+          <div className="mt-3">Service</div>
+          <div className="mt-3">Company</div>
         </div>
-        <div className='sb_footer-links_div'>
-            <h4>Coming soon</h4>
-             <div className='socialmedia'>
-                <p><img src={BsFacebook} alt='fb'></img></p>
-                <p><img src={BsTwitter} alt='twitter'></img></p>
-                <p><img src={BsLinkedin} alt='linkedin'></img></p>
-                <p><img src={BsInstagram} alt='instgram'></img></p>
-             </div>
-         </div>
+        <div className="column">
+          <span style={{ fontFamily: "Bebas Neue, sans-serif",marginTop:"20px" }}>Courses</span>
+          <div className="mt-3">Categories </div>
+          <div className="mt-3">Specialization course</div>
+          <div className="mt-3">Video Course</div>
+          <div className="mt-3">Live class</div>
+          <div className="mt-3">Tests</div>
         </div>
-        <hr></hr>
-        <div className='sb_footer-below'>
-         <div className='sb_footer-copyright'>
-           <p>
-            @{new Date().getFullYear()}codeInn. All right reserved.
-           </p>
-         </div>
-         <div className='sb_footer-below-links'>
-            <a href=''><div><p>Terms and conditon</p></div></a>
-            <a href=''><div><p>Privacy</p></div></a>
-            <a href=''><div><p>Security</p></div></a>
-            <a href=''><div><p>Cookie Declaration</p></div></a>
-         </div>
+        <div className="column">
+          <span style={{ fontFamily: "Bebas Neue, sans-serif" }}>Support</span>
+          <div className="mt-3">FAQ</div>
+          <div className="mt-3">Help Center</div>
+          <div className="mt-3">Populer Course</div>
+          <div className="mt-3">Career</div>
+          <div className="mt-3">Privacy</div>
+        </div>
+        <div className="column">
+          <span style={{ fontFamily: "Bebas Neue, sans-serif" }}>
+            ContactInfo
+          </span>
+          <div className="mt-3">+0913-705-3875</div>
+          <div className="mt-3">info@elearning.com</div>
+          <div className="mt-3">
+            4808 Skinner Hollow Road Days Creek, OR 97429
+          </div>
         </div>
       </div>
-    </div>
-  )
+
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
