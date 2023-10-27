dc936d0237964a0d2195ae695d97e5aca0b81c3c
Arshad <arshaarshad21@gmail.com>
2023-10-27 10:56:52 +0530

    update home and baner component

--- frontend/src/Containers/user/home/Banner.js
+++ frontend/src/Containers/user/home/Banner.js
@@ -1,7 +1,7 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import './Banner.css'
+import './Banner.css';
 
-import{ useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -13,29 +13,36 @@ const images = [
   // Add more image URLs as needed
 ];
 
-
-
-
 const ImageCarousel = () => {
+  const handleJoinNowClick = () => {
+    // Implement the logic for the "Join Now" button click
+    console.log('Join Now clicked');
+  };
+
+  const handleShowCourseClick = () => {
+    // Implement the logic for the "Show Course" button click
+    console.log('Show Course clicked');
+  };
+
   return (
     <div className="banner">
-      <div style={{height:'100px'}}>
+      <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt={`Image ${index + 1}`}
+              className="carousel-image"
+            />
+          </div>
+        ))}
+      </Carousel>
 
+      <div className="button-container">
+        <button onClick={handleJoinNowClick}>Join Now</button>
+        <button onClick={handleShowCourseClick}>Show Course</button>
       </div>
-  <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false}>
-    {images.map((image, index) => (
-      <div key={index}>
-        <img
-          src={image}
-          alt={`Image ${index + 1}`}
-          className="carousel-image"
-        />
-      </div>
-    ))}
-  </Carousel>
-
-</div>
-
+    </div>
   );
 };
 
