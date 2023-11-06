18ec26111d1647c5954c75582c4e85084e5490c2
Arshad <arshaarshad21@gmail.com>
2023-11-06 17:13:01 +0530

    Updated category mangement in  frontend admin side

--- frontend/src/Containers/admin/CategoryManagement.js
+++ frontend/src/Containers/admin/CategoryManagement.js
@@ -1,74 +1,156 @@
-import React ,{useState}from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import './UserManagement.css'
-import {AiOutlineAppstoreAdd} from 'react-icons/ai'
-import { adminInstance } from '../Utils/axios';
+import React, { useState, useEffect } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
+import { BiSolidEdit } from "react-icons/bi";
+
+import { adminInstance } from "../Utils/axios";
+import "./UserManagement.css";
+import CategoryAddModal from "./modal/CategoryAddModal";
+import EditCategoryModal from "./modal/EditCategoryModal"; // Import the new modal
+import { toast } from "react-toastify";
+
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'category_name', headerName: 'Category_name', width: 130 },
-  { field: 'image', headerName: 'image', width: 130 },
-  { field: 'is_active', headerName: 'Active', width: 130 },
-
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "category_name", headerName: "Category_name", width: 130 },
+  {
+    field: "image",
+    headerName: "Image",
+    width: 130,
+    renderCell: (params) => (
+      <img
+        src={`${params.value}`}
+        alt="Category"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+    ),
+  },
+  { field: "is_active", headerName: "Active", width: 130 },
 ];
 
-const rows = [
-  { id: 1, category_name: 'Programing', image: '',is_active:'active' },
-  { id: 2, category_name: 'UI/UX designing', image: '',is_active:'active'  },
-  { id: 3, category_name: 'Mathematics', image: '',is_active:'active' },
-  { id: 4, category_name: 'Science', image: '',is_active:'active'  },
-  { id: 5, category_name: 'DBMS', image: '', is_active:'active' },
-  { id: 6, category_name: 'RDBMS', image: '', is_active:'active' },
-  { id: 7, category_name: 'ML', image: '', is_active:'active' },
-  { id: 8, category_name: 'Cybersecurity', image: '', is_active:'active' },
-  { id: 9, category_name: 'Web developement', image: '',is_active:'active' },
-];
+const showToast = (message, type = "error") => {
+  toast[type](message, {
+    position: toast.POSITION.TOP_RIGHT,
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
 
-const CategoryManage = () => {
-   
-    const [newCategoryName, setNewCategoryName] = useState('');
-    const [newCategoryImage, setNewCategoryImage] = useState('');
-  
-    const handleAddCategory = async () => {
-      try {
-        // Send an HTTP POST request to the backend to add a new category
-        await adminInstance.post('your_backend_api_url', {
-          category_name: newCategoryName,
-          image: newCategoryImage,
-          // other fields as needed
-        });
-  
-        // Refresh the data or perform any other necessary actions
-      } catch (error) {
-        console.error('Error adding category', error);
-      }
-    };
+const CategoryManagement = () => {
+  const [categories, setCategories] = useState([]);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
+  const fetchCategories = async () => {
+    try {
+      const response = await adminInstance.get("/categories/");
+      setCategories(response.data);
+    } catch (error) {
+      console.error("Error fetching categories", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const handleAddCategory = async (categoryData) => {
+    try {
+      await adminInstance.post("/create-categories/", categoryData);
+      fetchCategories();
+      showToast("Category added", "success");
+      setIsAddModalOpen(false);
+    } catch (error) {
+      showToast("Error adding category", "error");
+      console.error("Error adding category", error);
+    }
+  };
+
+  const handleEditCategory = (category) => {
+    setSelectedCategory(category);
+    setIsEditModalOpen(true);
+  };
+
+  const handleUpdateCategory = async (updatedCategoryData, categoryId) => {
+    try {
+      await adminInstance.put(`/categories/${categoryId}/`, updatedCategoryData);
+      fetchCategories();
+      showToast("Category updated", "success");
+      setIsEditModalOpen(false);
+    } catch (error) {
+      showToast("Error updating category", "error");
+      console.error("Error updating category", error);
+    }
+  };
+  
+
+  const columnsWithActions = [
+    ...columns,
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 130,
+      renderCell: (params) => (
+        <div>
+          <button
+            onClick={() => handleEditCategory(params.row)}
+            style={{ border: "none", background: "none", cursor: "pointer" }}
+          >
+            <BiSolidEdit style={{ fontSize: '24px' ,color:"blue"}} />
+          </button>{" "}
+        </div>
+      ),
+    },
+  ];
 
   return (
-    <div className="data-grid-container">
-         <div className="header d-flex justify-content-between align-items-center mb-4">
-         <div style={{ fontWeight: 'bold' }}>
-               CategoryManagement
-         </div>
-          <div className="d-flex align-items-center">
-            <AiOutlineAppstoreAdd style={{fontSize: '30px'}}/> Add
+    <div style={{ backgroundColor: "pink", height: "100vh" }}>
+      <div className="data-grid-container">
+        <div className="header d-flex justify-content-between align-items-center mb-4">
+          <div style={{ fontWeight: "bold" }}>Category Management</div>
+          <div
+            className="d-flex align-items-center"
+            onClick={() => setIsAddModalOpen(true)}
+          >
+            <AiOutlineAppstoreAdd style={{ fontSize: "30px" }} /> Add
           </div>
         </div>
-      <div className="h-96 w-full overflow-hidden border border-gray-300">
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
+        <div className="h-500 w-full overflow-hidden border border-gray-300">
+          <DataGrid
+            rows={categories}
+            columns={columnsWithActions}
+            pageSize={5}
+            checkboxSelection
+            sx={{ backgroundColor: "white" }}
+            isCellEditable={(params) => params.field !== "id"}
+            onCellEditCommit={(params) => {
+              const updatedData = [...categories];
+              updatedData[params.id - 1][params.field] = params.props.value;
+              handleUpdateCategory(updatedData[params.id - 1]);
+            }}
+          />
+        </div>
+
+        <CategoryAddModal
+          isOpen={isAddModalOpen}
+          onRequestClose={() => setIsAddModalOpen(false)}
+          onAddCategory={handleAddCategory}
         />
+
+<EditCategoryModal
+  isOpen={isEditModalOpen}
+  onRequestClose={() => setIsEditModalOpen(false)}
+  onUpdateCategory={(updatedData) => handleUpdateCategory(updatedData, selectedCategory.id)}
+  categoryData={selectedCategory}
+/>
       </div>
     </div>
   );
 };
 
-export default CategoryManage;
+export default CategoryManagement;
