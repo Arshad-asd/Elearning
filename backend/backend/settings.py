57f7535069db142e2e8cb76b0e5b044062d3729e
Arshad <arshaarshad21@gmail.com>
2023-11-10 19:02:52 +0530

    Delete subscription app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'course',
-    'subscription',
     'accounts.apps.AccountsConfig',
     'rest_framework',
     'corsheaders',
