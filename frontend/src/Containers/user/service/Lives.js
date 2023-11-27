3bb6749bba417aa31f07ad1754a7cf45ef97e0d6
Arshad <arshaarshad21@gmail.com>
2023-11-27 10:30:06 +0530

    lives update in frontend

--- frontend/src/Containers/user/service/Lives.js
+++ frontend/src/Containers/user/service/Lives.js
@@ -3,9 +3,9 @@ import LiveCard from '../../../Components/Cards/UserCard/LiveCard'
 
 function Lives() {
   return (
-    <div className=' w-full  pt-4 bg-cyan-100' style={{height:'100vh',}}>
-      <div className='container grid grid-cols-4'>
-      <LiveCard />
+    <div className=' w-full pt-4 bg-cyan-100' style={{height:'100vh',}}>
+      <div className='container grid xl:grid-cols-4  lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
+      <LiveCard/>
       <LiveCard />
       <LiveCard />
       <LiveCard />
